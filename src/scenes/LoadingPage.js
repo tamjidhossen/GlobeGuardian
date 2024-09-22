13fcfaddeac142ce62040bcd467ec180c9af4f05
tam1mBD <st.tamim.2001@gmail.com>
2024-09-22 10:25:23 +0600

    minor change in folder structure

---
+++ src/scenes/LoadingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native'
+
+class LoadingPage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Loading Page</Text>
+      </View>
+    );
+  }
+}
+
+export default LoadingPage;
---
+++ src/scenes/MainMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MainMap extends Component {
+  render() {
+    return (
+      <text> Main map</text>
+    );
+  }
+}
+
+
+export default MainMap;
